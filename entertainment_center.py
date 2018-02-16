9b345f092b42c51818c473d16711b2aba176472f
Johannes Hettig <johannes.hettig@gmail.com>
2018-02-16 21:42:42 +0800

    Created Movie class with 3 movies

---
+++ entertainment_center.py
@@ -0,0 +1,20 @@
+import media
+import fresh_tomatoes
+
+
+# List of Movies
+# It actually better to get this from a database
+StarTrekXI = media.Movie('Star Trek XI', 'Bad guy comes from the future and changes the past',
+                       'https://upload.wikimedia.org/wikipedia/en/2/29/Startrekposter.jpg',
+                       'https://www.youtube.com/watch?v=pKFUZ10Wmbw')
+
+StarTrekXII = media.Movie('Star Trek XII', 'Strong guys wants to get his family back - and rule the world',
+                          'https://upload.wikimedia.org/wikipedia/en/5/50/StarTrekIntoDarkness_FinalUSPoster.jpg',
+                          'https://www.youtube.com/watch?v=GOQMXNwp8wo')
+
+StarTrekXIII = media.Movie('Star Trek XIII', 'Stranded on a lost planet fighting the way back out',
+                           'https://upload.wikimedia.org/wikipedia/en/b/ba/Star_Trek_Beyond_poster.jpg',
+                           'https://www.youtube.com/watch?v=fLVPfbs19kc')
+
+movies = [StarTrekXI, StarTrekXII, StarTrekXIII]
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,8 @@
+
+
+class Movie():
+    def __init__(self, movie_title, movie_storyline, poster_image, movie_trailer):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = movie_trailer
