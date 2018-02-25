c7502239ba30f297a5b2b5ed88f5e743678d368b
Johannes Hettig <johannes.hettig@gmail.com>
2018-02-26 07:52:34 +0800

    Adding better comments. Add docstring to class movie.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,18 +1,37 @@
+##########################################
+# Project: Movie Website
+# Date Started: 1/1/2018
+# Date Completed: pending
+# Submitted by: Johannes Hettig
+##########################################
+
+######################################## Media File ####################################################
+# Description: This file instantiates the class Movie to be used in the fresh_tomatoes.py html generator.
+# The main source for the structure of the file was obtained from Udacity FDND Course
+########################################################################################################
+
 import media
 import fresh_tomatoes
 
 
 # List of Movies
 # It actually better to get this from a database
-StarTrekXI = media.Movie('Star Trek XI', 'Bad guy comes from the future and changes the past',
-                       'https://upload.wikimedia.org/wikipedia/en/2/29/Startrekposter.jpg',
-                       'https://www.youtube.com/watch?v=3PM1pvOzn_w')
 
-StarTrekXII = media.Movie('Star Trek XII', 'Strong guys wants to get his family back - and rule the world',
+# Instantiating class movie for movie "Star Trek XI"
+StarTrekXI = media.Movie('Star Trek XI',
+                        'Bad guy comes from the future and changes the past',
+                        'https://upload.wikimedia.org/wikipedia/en/2/29/Startrekposter.jpg',
+                        'https://www.youtube.com/watch?v=3PM1pvOzn_w')
+
+# Instantiating class movie for movie "Star Trek XII"
+StarTrekXII = media.Movie('Star Trek XII',
+                          'Strong guys wants to get his family back - and rule the world',
                           'https://upload.wikimedia.org/wikipedia/en/5/50/StarTrekIntoDarkness_FinalUSPoster.jpg',
                           'https://www.youtube.com/watch?v=r5gdbUC9mWU')
 
-StarTrekXIII = media.Movie('Star Trek XIII', 'Stranded on a lost planet fighting the way back out',
+# Instantiating class movie for movie "Star Trek XIII"
+StarTrekXIII = media.Movie('Star Trek XIII',
+                           'Stranded on a lost planet fighting the way back out',
                            'https://upload.wikimedia.org/wikipedia/en/b/ba/Star_Trek_Beyond_poster.jpg',
                            'https://www.youtube.com/watch?v=fLVPfbs19kc')
 
--- media.py
+++ media.py
@@ -1,7 +1,34 @@
+##########################################
+# Project: Movie Website
+# Date Started: 1/1/2018
+# Date Completed: pending
+# Submitted by: Johannes Hettig
+##########################################
+
+######################################## Media File ####################################################
+# Description: This file creates the class Movie to allow for instances of this class to be used in the
+#              entertainment.py file. This definition of the class Movie was obtained through the course
+########################################################################################################
 
 
-# Simple class with 4 instance variable
 class Movie():
+    """
+    The class Movie lets other functions use four class variables:
+
+    At this point no further fuctions are implemented in this class.
+
+    Args:
+        movie_title: The title of the movie.
+        movie_storyline: The storyline of the movie.
+        poster_image: The URL to the image of the movie.
+        movie_trailer: The URL to the trailer video of the movie.
+
+    Returns:
+        no returns
+
+    Raises:
+        no execptions
+    """
     def __init__(self, movie_title, movie_storyline, poster_image, movie_trailer):
         self.title = movie_title
         self.storyline = movie_storyline
